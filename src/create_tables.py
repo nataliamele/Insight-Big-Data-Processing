70eb4a3610078b0e5067e5b36d1104aaaa35b8f4
Natalia Meleshkina <mnnbox@gmail.com>
2019-09-27 09:20:29 -0700

    add sql queries

---
+++ src/create_tables.py
@@ -0,0 +1,36 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def create_database():
+    # connect to default database
+    conn = psycopg2.connect("host=10.0.0.4 dbname=aotdb user= password=")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    return cur, conn
+
+
+def drop_tables(cur, conn):
+    # drop tables
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    cur, conn = create_database()
+
+    # drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
