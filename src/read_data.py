4f0a9f5f21ec571a58304170e6343babfb7e734e
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-18 05:43:21 -0700

    read from DB

--- src/read_data.py
+++ src/read_data.py
@@ -8,27 +8,40 @@ from pyspark.sql.types import *
 
 if __name__ == "__main__":
 
-    sensors_csv = "s3://insight-natnm/data/sensors.csv"
-    nodes_csv = "s3://insight-natnm/data/nodes.csv"
+    # sensors_csv = "s3://insight-natnm/data/sensors.csv"
+    # nodes_csv = "s3://insight-natnm/data/nodes.csv"
 
     spark = SparkSession\
             .builder\
             .appName('ReadPG')\
             .getOrCreate()
 
-    df_sensors = spark \
-        .read \
-        .option("header", "true") \
-        .option("inferSchema", "true") \
-        .option("charset", "UTF-8")\
-        .csv(sensors_csv)
 
-    df_nodes = spark \
-        .read \
-        .option("header", "true") \
-        .option("inferSchema", "true") \
-        .option("charset", "UTF-8")\
-        .csv(nodes_csv)
+    df_sensors = spark.read\
+          .format("jdbc")\
+          .options(\
+              driver="org.postgresql.Driver", \
+              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
+              dbtable="public.sensors",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
+          .load()
     
-    df_sensors.take(10).show()
-    df_nodes.take(10).show()
+    df_obs = spark.read\
+          .format("jdbc")\
+          .options(\
+              driver="org.postgresql.Driver", \
+              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
+              dbtable="public.observations2",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
+          .load()
+    
+    df_nodes = spark.read\
+          .format("jdbc")\
+          .options(\
+              driver="org.postgresql.Driver", \
+              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
+              dbtable="public.nodes",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
+          .load()
+    
+    df_sensors.show()
+    df_obs.take(10).show()
+    # df_nodes.take(10).show()
+    
