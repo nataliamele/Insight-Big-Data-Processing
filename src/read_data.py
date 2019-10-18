35a31da9008dfb98fa29ff1bffef546e74fb7c9b
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-18 09:17:59 -0700

    df joining

--- src/read_data.py
+++ src/read_data.py
@@ -10,15 +10,25 @@ from pyspark.sql.types import *
 def read_from_db(db_name):
 
     df = spark.read\
-            .format("jdbc")\
-            .option("header", "true") \
-            .option("inferSchema", "true") \
-            .options(\
-                driver="org.postgresql.Driver", \
-                url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
-                dbtable=db_name,user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
-            .load()
+        .format("jdbc")\
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .options(\
+            driver="org.postgresql.Driver", \
+            url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
+            dbtable=db_name,user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
+        .load()
     return df
+
+def read_from_s3(path):
+
+    df = spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .option("charset", "UTF-8")
+        .csv(path)
+    return df
+
 
 if __name__ == "__main__":
 
@@ -26,17 +36,26 @@ if __name__ == "__main__":
     # nodes_csv = "s3://insight-natnm/data/nodes.csv"
 
     spark = SparkSession\
-            .builder\
-            .appName('ReadPG')\
-            .getOrCreate()
+        .builder\
+        .appName('ReadPG')\
+        .getOrCreate()
 
     # Sensors dataframe
-    df_sensors = read_from_db('public.sensors')\
+    # df_sensors = read_from_db('public.sensors')\
+    #     .select( 'sensor_path','sensor_measure','hrf_unit','hrf_max')
+    # df_sensors.show().take(5)
+
+    df_sensors = read_from_s3("s3://insight-natnm/data/sensors.csv")\
         .select( 'sensor_path','sensor_measure','hrf_unit','hrf_max')
     df_sensors.show().take(5)
 
+
     # Nodes dataframe
-    df_nodes = read_from_db('public.nodes')\
+    # df_nodes = read_from_db('public.nodes')\
+    #     .select('node_id','vsn','lat', 'lon', 'community_area')
+    # df_nodes.show().take(5)
+
+    df_nodes = read_from_s3("s3://insight-natnm/data/nodes.csv")\
         .select('node_id','vsn','lat', 'lon', 'community_area')
     df_nodes.show().take(5)
 
