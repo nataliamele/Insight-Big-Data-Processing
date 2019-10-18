b6a72956458278a22fe935d4566f0b717970d301
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-18 09:09:54 -0700

    df joining

--- src/read_data.py
+++ src/read_data.py
@@ -30,22 +30,25 @@ if __name__ == "__main__":
             .appName('ReadPG')\
             .getOrCreate()
 
-
+    # Sensors dataframe
     df_sensors = read_from_db('public.sensors')\
         .select( 'sensor_path','sensor_measure','hrf_unit','hrf_max')
-    df_sensors.show().take(10)
+    df_sensors.show().take(5)
 
-    df_obs = read_from_db('public.observations2')\
-        .select('ts','node_id','sensor_path','value_hrf')
-    df_obs.show().take(10)
-
+    # Nodes dataframe
     df_nodes = read_from_db('public.nodes')\
         .select('node_id','vsn','lat', 'lon', 'community_area')
-    df_nodes.show().take(10)
+    df_nodes.show().take(5)
 
+    # Observations dataframe
+    df_obs = read_from_db('public.observations2')\
+        .select('ts','node_id','sensor_path','value_hrf')
+    df_obs.show().take(5)
+
+    # Enreach observation dataframe
     df_result= df_obs.join(df_nodes,df_obs("node_id") == df_nodes("vsn"), how ='left')\
         .select('ts','node_id','sensor_path','value_hrf','lat', 'lon', 'community_area')
 
-    df_result.show().take(10)
+    df_result.show().take(5)
     # df_nodes.take(10).show()
     
