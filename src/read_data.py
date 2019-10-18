28482c4fe570d92c276c79bc303fbc20f39611f2
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-18 08:57:53 -0700

    df joining

--- src/read_data.py
+++ src/read_data.py
@@ -6,6 +6,20 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+
+def read_from_db(db_name):
+
+    df = spark.read\
+            .format("jdbc")\
+            .option("header", "true") \
+            .option("inferSchema", "true") \
+            .options(\
+                driver="org.postgresql.Driver", \
+                url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
+                dbtable=db_name,user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
+            .load()\
+    return df
+
 if __name__ == "__main__":
 
     # sensors_csv = "s3://insight-natnm/data/sensors.csv"
@@ -17,32 +31,18 @@ if __name__ == "__main__":
             .getOrCreate()
 
 
-    df_sensors = spark.read\
-          .format("jdbc")\
-          .options(\
-              driver="org.postgresql.Driver", \
-              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
-              dbtable="public.sensors",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
-          .load()
+    df_sensors = read_from_db(public.sensors)\
+        .select( 'sensor_path','sensor_measure','hrf_unit','hrf_max')
     
-    df_obs = spark.read\
-          .format("jdbc")\
-          .options(\
-              driver="org.postgresql.Driver", \
-              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
-              dbtable="public.observations2",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
-          .load()
+    df_obs = read_from_db(public.sensors)\
+        .select('ts','node_id','sensor_path','value_hrf')
     
-    df_nodes = spark.read\
-          .format("jdbc")\
-          .options(\
-              driver="org.postgresql.Driver", \
-              url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
-              dbtable="public.nodes",user=os.environ['DB_USER'], password=os.environ['DB_PWD'])\
-          .load()
+    df_nodes = read_from_db(public.sensors)\
+        .select('node_id','vsn','lat', 'lon', 'community_area')
     
-    df_result = df_obs.join(df_nodes,df_obs("node_id") == df_nodes("vsn"), how ='left').select('ts','node_id','sensor_path','value_hrf','lat', 'lon', 'community_area')
+    df_result= df_obs.join(df_nodes,df_obs("node_id") == df_nodes("vsn"), how ='left')\
+        .select('ts','node_id','sensor_path','value_hrf','lat', 'lon', 'community_area')
 
-    df.show().take(10)
+    df_result.show().take(10)
     # df_nodes.take(10).show()
     
