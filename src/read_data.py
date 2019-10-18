18aa6541b7d2441e83c85fc66b44464cabf22f1c
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-18 09:40:24 -0700

    df joining

--- src/read_data.py
+++ src/read_data.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     # df_obs.show()
 
     # Enreach observation dataframe
-    df_result= df_obs.join(df_nodes, df_obs["node_id"] == df_nodes["vsn"], how ='left')\
+    df_result= df_obs.join(df_nodes, df_obs.node_id == df_nodes.vsn, how ='left')\
         .select('ts','node_id','sensor_path','value_hrf','lat', 'lon', 'community_area')
 
     df_result.show()
