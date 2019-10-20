7a4ed0cbec42a96363aac6b37473f30c97fc666a
Natalia Meleshkina <mnnbox@gmail.com>
2019-10-20 07:00:23 -0700

    comments edit

--- src/consumer2.py
+++ src/consumer2.py
@@ -12,15 +12,16 @@ from pyspark.sql.functions import *
 from pyspark.sql.functions import udf, col, to_timestamp, round
 from pyspark.sql.types import *
 
-def read_from_db(db_name):
+def read_from_db(table_name):
     '''
-    Connects to Postgres and reads table into df
+    Connects to Postgres and reads table into df.
+    To use your own schema use .schema(schema) instead of ' .option("inferSchema", "true") ' and add to to func parameters
     Returns df
     '''
     df = spark.read\
         .format("jdbc")\
-        .option("header", "true") \
         .option("inferSchema", "true") \
+        .option("header", "true") \
         .options(\
             driver="org.postgresql.Driver", \
             url="jdbc:postgresql://10.0.0.4:5342/aotdb", \
@@ -49,12 +50,18 @@ if __name__ == "__main__":
         .builder\
         .appName("SensorsStream")\
         .getOrCreate()
-    
+
+    # schema_sensors = StructType([ StructField("sensor_path", StringType())\
+    #                             , StructField("node_id", StringType())\
+    #                             , StructField("sensor_path", StringType())\
+    #                             , StructField("value_hrf", FloatType())\
+    #                          ])   
+
     # Create sensors dataframe from Postgres table
     df_sensors = read_from_db('public.sensors')\
         .select('sensor_path','sensor_measure','hrf_unit','hrf_max')
     
-    # Create nofes dataframe    
+    # Create nodes dataframe    
     df_nodes = read_from_db('public.nodes')\
         .select('vsn','lat', 'lon', 'community_area')\
         .withColumn('lat', round(col('lat'), 2))\
@@ -85,7 +92,7 @@ if __name__ == "__main__":
             get_json_object(df.value, "$.sensor_path").cast(StringType()).alias("sensor_path"),\
             get_json_object(df.value, "$.value_hrf").cast(FloatType()).alias("value_hrf")\
             )
-    
+
     # Filter Chemsense and Aphasense sensors
     df_obs = df_parsed \
         .filter((col('sensor_path').like('chemsense.%')) | (col('sensor_path').like('alphasense.opc_n2.pm%')) )\
@@ -93,9 +100,9 @@ if __name__ == "__main__":
         .withColumn('value_hrf', round(col('value_hrf'), 2))
 
     # Enreach observation streaming df
-    df_result= df_obs.join(df_nodes, df_obs.node_id == df_nodes.vsn, how='left')\
-        .join(df_sensors, df_obs.sensor_path == df_sensors.sensor_path, how='left').drop(df_sensors.sensor_path) 
-    #    .select('ts','node_id','sensor_path','value_hrf','hrf_unit','lat', 'lon', 'community_area')
+    df_result= df_obs.join(df_nodes, df_obs.node_id == df_nodes.vsn, 'left_outer')\
+        .join(df_sensors, df_obs.sensor_path == df_sensors.sensor_path, 'left_outer').drop(df_sensors.sensor_path) 
+        .select('ts','node_id','sensor_path','value_hrf','hrf_unit','lat', 'lon', 'community_area')
 
 
     ## write to TimescaleDB 
